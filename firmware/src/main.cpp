563abf6df9ee84eb34f1761083110f2ac5c7ee72
krizdingus <contact@krizdingus.com>
2026-05-13 11:36:43 -0400

    bring up ILI9341 display via LovyanGFX

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -4,10 +4,26 @@
 
 #include <Arduino.h>
 
+#include "hw/display.h"
+
 void setup() {
   Serial.begin(115200);
-  delay(500);
+  delay(200);
   Serial.println("cydmonitor firmware booting");
+
+  auto &lcd = cyd::display();
+  lcd.init();
+  lcd.setRotation(0);
+  lcd.setBrightness(200);
+
+  lcd.fillScreen(0x0000);                // black
+  lcd.fillRect(0, 0, 240, 80, 0xF800);   // red top
+  lcd.fillRect(0, 80, 240, 80, 0x07E0);  // green middle
+  lcd.fillRect(0, 160, 240, 80, 0x001F); // blue
+  lcd.fillRect(0, 240, 240, 80, 0xFFE0); // yellow bottom
+  lcd.setTextColor(0xFFFF, 0x0000);
+  lcd.setCursor(20, 300);
+  lcd.printf("CYD display OK");
 }
 
 void loop() {
