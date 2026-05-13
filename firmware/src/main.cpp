283f24261c0ccc09a026326c2af2a798d4d0bd4a
krizdingus <contact@krizdingus.com>
2026-05-13 12:20:12 -0400

    wire state machine to UI and network I/O

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,32 +3,17 @@
 #ifndef UNIT_TEST
 
 #include <Arduino.h>
-#include <lvgl.h>
 
-#include "hw/display.h"
-#include "hw/touch.h"
-#include "ui/lvgl_glue.h"
+#include "app/app_loop.h"
 
 void setup() {
   Serial.begin(115200);
-  cyd::display().init();
-  cyd::display().setRotation(0);
-  cyd::display().setBrightness(200);
-  cyd::display().fillScreen(0x0000);
-  cyd::touch().probe_and_init();
-  cyd::lvgl_init();
-
-  lv_obj_t *scr = lv_screen_active();
-  lv_obj_set_style_bg_color(scr, lv_color_hex(0x0E0E10), 0);
-  lv_obj_t *label = lv_label_create(scr);
-  lv_label_set_text(label, "LVGL OK\nclaude monitor");
-  lv_obj_set_style_text_color(label, lv_color_hex(0xD97757), 0);
-  lv_obj_set_style_text_font(label, &lv_font_montserrat_24, 0);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  delay(200);
+  cyd::app_init();
 }
 
 void loop() {
-  cyd::lvgl_tick();
+  cyd::app_tick();
   delay(5);
 }
 
